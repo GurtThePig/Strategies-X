0f96e2b06f147b2e3decd0c03b229342f6bd8414
Gurty <synergysypher@gmail.com>
2024-12-17 12:05:50 +0700

    Update Map.lua

--- TDS/TDSTools/Functions/Map.lua
+++ TDS/TDSTools/Functions/Map.lua
@@ -15,6 +15,7 @@ local SpecialGameMode = {
     ["The Nightmare Realm"] = {mode = "halloween2024", difficulty = "Act2", night = 2},
     ["Containment"] = {mode = "halloween2024", difficulty = "Act3", night = 3},
     ["Pls Donate"] = {mode = "plsDonate", difficulty = "PlsDonateHard"},
+    ["Outpost 32"] = {mode = "frostInvasion", difficulty = "Hard" },
     --Temporary Special Maps ^^^^^^
     ["Classic Candy Cane Lane"] = {mode = "Event", part = "ClassicRobloxPart1"},
     ["Classic Winter"] = {mode = "Event", part = "ClassicRobloxPart2"},
@@ -32,6 +33,7 @@ local ElevatorSettings = {
     ["Halloween2024"] = {Enabled = false},
     ["PlsDonate"] = {Enabled = false},
     ["Event"] = {Enabled = false},
+    ["FrostInvasion"] = {Enabled = false}
 }
 
 return function(self, p1)
@@ -118,6 +120,12 @@ return function(self, p1)
                         ["difficulty"] = SpecialTable.difficulty,
                         ["count"] = 1,
                         ["mode"] = SpecialTable.mode,
+                    })
+                elseif SpecialTable.mode == "frostInvasion" then
+                    RemoteFunction:InvokeServer("Multiplayer","v2:start",{
+                        ["difficulty"] = SpecialTable.difficulty,
+                        ["mode"] = SpecialTable.mode,
+                        ["count"] = 1,
                     })
                 elseif SpecialTable.mode == "Event" then
                     RemoteFunction:InvokeServer("EventMissions","Start", SpecialTable.part)
