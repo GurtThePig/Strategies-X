5cd6dfb440090c1e09fa20ebe535f331ae8afc3e
Gurty <synergysypher@gmail.com>
2024-12-21 09:40:03 +0700

    Update Upgrade.lua

--- TDS/TDSTools/Functions/Upgrade.lua
+++ TDS/TDSTools/Functions/Upgrade.lua
@@ -33,11 +33,6 @@ return function(self, p1)
         task.delay(50, function()
             SkipCheck = true
         end)
-        if Path == 2 then
-            TowersContained[Tower].BottomPathUpgrade += 1
-        elseif Path == 1 then
-            TowersContained[Tower].TopPathUpgrade += 1
-        end
         repeat
             if not TowersCheckHandler(Tower) then
                 prints("End Upgrade",Wave, Min, Sec, InWave)
@@ -49,6 +44,11 @@ return function(self, p1)
             })
             task.wait()
         until UpgradeCheck or SkipCheck
+        if Path == 2 then
+            TowersContained[Tower].BottomPathUpgrade += 1
+        elseif Path == 1 then
+            TowersContained[Tower].TopPathUpgrade += 1
+        end
         local TowerType = GetTypeIndex(tableinfo["TypeIndex"],Tower)
         if CurrentCount ~= StratXLibrary.RestartCount then
             return
