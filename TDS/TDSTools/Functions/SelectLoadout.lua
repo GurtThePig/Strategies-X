6d3736f7e94d4fa3d0536d196c47183da9436558
Gurty <synergysypher@gmail.com>
2024-12-28 16:41:53 +0700

    Update SelectLoadout.lua

--- TDS/TDSTools/Functions/SelectLoadout.lua
+++ TDS/TDSTools/Functions/SelectLoadout.lua
@@ -2,12 +2,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 return function(self, p1)
     local tableinfo = p1
-    local GameSetLoadoutName = tableinfo["GameSetLoadout"]
+    local GameSetLoadoutName = `"{tableinfo["GameSetLoadout"]}"`
+    local GameSetLoadoutName2 = tableinfo["GameSetLoadout"]
+    print(GameSetLoadoutName, GameSetLoadoutName2)
     if not CheckPlace() then
         return
     end
     task.spawn(function()
-        ReplicatedStorage.Network.PlayerManager.SelectLoadout:FireServer(`"{GameSetLoadoutName}"`)
+        ReplicatedStorage.Network.PlayerManager.SelectLoadout:FireServer(GameSetLoadoutName)
         ConsoleInfo("Choosen Loadout: "..GameSetLoadoutName)
     end)
 end
