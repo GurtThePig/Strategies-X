774d2efa06d36b64a926a6b34b62e697a7b7542f
Gurty <synergysypher@gmail.com>
2024-12-28 15:51:55 +0700

    Update MainSource.lua

--- TDS/MainSource.lua
+++ TDS/MainSource.lua
@@ -648,8 +648,8 @@ if CheckPlace() then
                 task.wait()
             until VoteGUI:WaitForChild("count").Text == `0/{#Players:GetChildren()} Required`
 		end
-		if VoteGUI.Position ~= UDim2.new(0.5, 0, 0.5, 0) then --UDim2.new(scale_x, offset_x, scale_y, offset_y)
-			return
+		if VoteGUI.Position ~= UDim2.new(2, 30, 0.5, 0) then --UDim2.new(scale_x, offset_x, scale_y, offset_y)
+			task.wait(.2)
 		end
 		local currentPrompt = VoteGUI:WaitForChild("prompt").Text
    		if currentPrompt == "Ready?" or currentPrompt == "Skip Cutscene?" then --Event GameMode
